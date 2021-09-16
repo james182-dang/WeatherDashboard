332b9bd150d5f614d01f001670f0053927aa9095
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-09-16 14:10:20 -0400

    add additional API, current weather info display

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -6,13 +6,50 @@ let citySearchBtn = $("#citySearchBtn");
 function citySearch() {
 
     let citySearchValue = $("#citySearchBar").val().trim();
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + citySearchValue + "&appid=2f0eea858680841d20a1b82aaa9fa729")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + citySearchValue + "&units=imperial&appid=2f0eea858680841d20a1b82aaa9fa729")
         .then(function(response) {
             return response.json();
         })
         .then(function(response) {
-            let responseHeader = document.querySelector("#response-header");
-            responseHeader.innerHTML = "<h2>" + citySearchValue + "</h2>";
+
+            let lon = response.coord.lon;
+            let lat = response.coord.lat;
+
+            fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&units=imperial&appid=2f0eea858680841d20a1b82aaa9fa729")
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(response) {
+
+                    let responseHeader = document.querySelector("#response-header");
+                    responseHeader.innerHTML = "<h2>" + citySearchValue + "</h2>";
+        
+                    let weatherDetails = document.createElement("div");
+                    weatherDetails.id = 'details';
+                    weatherDetails.className = 'details';
+        
+                    responseHeader.appendChild(weatherDetails);
+        
+                    let detailsDiv = document.createElement("div")
+        
+                    let temp = response.current.temp;
+                    let wind = response.current.wind_speed;
+                    let humidity = response.current.humidity;
+                    let uvi = response.current.uvi;
+        
+        
+        
+                    detailsDiv.innerHTML = "<h2> Temp: " + temp + "</h2>" +
+                    "<br />" + 
+                    "<h2> Wind Speed: " + wind + "</h2>" + 
+                    "<br />" +
+                    "<h2> Humidity: " + humidity + "</h2>" +
+                    "<br />" +
+                    "<h2> UV Index: " + uvi + "</h2>";
+                    
+                    weatherDetails.appendChild(detailsDiv);
+
+                })
 
         })
 }
